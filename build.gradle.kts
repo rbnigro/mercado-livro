a2d63650a6672c4c7d97804a506d7885e093e03e
Ronney Nigro <ronneynigro@gmail.com>
2021-12-08 16:30:45 -0300

    Implements Swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,25 @@ repositories {
 }
 
 dependencies {
-	// implementation("org.jetbrains.kotlin:kotlin-reflect:1.4")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+
 	implementation("org.flywaydb:flyway-core:8.2.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	runtimeOnly("mysql:mysql-connector-java")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/com/mercadolivro/controller/request/PurchaseRequest.kt
@@ -0,0 +1,13 @@
+package com.mercadolivro.controller.request
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+
+data class PurchaseRequest (
+    @field:NotNull
+    @field:Positive
+    val customerId: Int,
+
+    @field:NotNull
+    val bookIds: Set<Int> // permite id´s duplicados
+        )
