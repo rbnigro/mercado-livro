28974a3d0af28826167684b5fad8c88f9f826acc
Ronney Nigro <ronneynigro@gmail.com>
2021-12-16 08:36:20 -0300

    Mockar Classe Estática

---
+++ src/test/kotlin/com/mercadolivro/events/listener/GenerateNfeListenerTest.kt
@@ -0,0 +1,38 @@
+package com.mercadolivro.events.listener
+
+import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.helper.buildPurchase
+import com.mercadolivro.service.PurchaseService
+import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.util.*
+
+@ExtendWith(MockKExtension::class)
+class GenerateNfeListenerTest{
+
+    @MockK
+    private lateinit var purchaseService: PurchaseService
+
+    @InjectMockKs
+    private lateinit var generateNfeListener: GenerateNfeListener
+
+    @Test
+    fun `should geneterate nfe`() {
+        val purchase = buildPurchase(nfe = null)
+        val fakeNFE = UUID.randomUUID()
+        val purchaseExpected = purchase.copy(nfe = fakeNFE.toString())
+
+        mockkStatic(UUID::class) // mockar retorno
+
+        every { UUID.randomUUID() } returns fakeNFE
+        every { purchaseService.update(purchaseExpected) } just runs
+
+        generateNfeListener.listen(PurchaseEvent(this, purchase))
+
+        verify(exactly = 1) { purchaseService.update(purchaseExpected)  }
+    }
+}
